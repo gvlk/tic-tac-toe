4c268415c45c7fa85211e9180f572393e70a0d82
Gui <88752991+gvlk@users.noreply.github.com>
2023-12-27 01:18:42 -0300

    code refactor, implemented mark class, improved performance by using sprites

--- src/game_controller.py
+++ src/game_controller.py
@@ -1,10 +1,11 @@
+from typing import Optional, List, Set, Tuple
+
 import pygame as pg
 import pygame.freetype
 
 from src.modules.debugger import Debugger
 from src.modules.mouse import Mouse
-
-from typing import Optional, List, Set
+from src.modules.mark import Mark
 
 
 class GameController:
@@ -46,12 +47,11 @@ class GameController:
 
         self.grid_rects: List[pg.Rect] = self.generate_grid_rects()
 
-        original_x_surf: pg.Surface = pg.image.load("src/assets/imgs/x.png")
-        original_o_surf: pg.Surface = pg.image.load("src/assets/imgs/o.png")
-        self.x_surf: pg.Surface = pg.transform.scale(original_x_surf, self.grid_rects[0].size)
-        self.o_surf: pg.Surface = pg.transform.scale(original_o_surf, self.grid_rects[0].size)
-        self.x_rect: pg.Rect = self.x_surf.get_rect()
-        self.o_rect: pg.Rect = self.o_surf.get_rect()
+        # TODO: Make a module to deal with image files like the background. The other parts should request the file
+        #  to this module to get it.
+
+        self.bg_surf, self.bg_rect = self.generate_background()
+        self.marks = pg.sprite.Group()
 
         self.p1_win_conditions: Set[str] = self.P1_WIN_CONDITIONS.copy()
         self.p2_win_conditions: Set[str] = self.P2_WIN_CONDITIONS.copy()
@@ -61,6 +61,14 @@ class GameController:
         self.game_state: str = self.EMPTY_CELL * 9
         self.finished: bool = False
         self.winner: str = self.EMPTY_CELL
+
+        self.turn_text, self.turn_text_rect = self.generate_turn_message()
+        (
+            self.finished_text,
+            self.finished_text_rect,
+            self.reset_text,
+            self.reset_text_rect
+        ) = self.generate_winner_message()
 
         self.run: bool = True
 
@@ -116,10 +124,33 @@ class GameController:
             if clicked_cell is not None:
                 self.turn += 1
                 self.game_state = self.game_state[:clicked_cell] + self.turn_player + self.game_state[clicked_cell + 1:]
+                self.add_mark_sprite(clicked_cell)
                 self.switch_turn()
                 self.winner = self.check_win()
                 if self.winner != self.EMPTY_CELL:
-                    self.finished = True
+                    self.finish_game()
+
+    def add_mark_sprite(self, clicked_cell: int) -> None:
+        """
+        Add a mark sprite to the group based on the clicked cell.
+        :param clicked_cell: The index of the clicked cell.
+        """
+        mark_position = self.grid_rects[clicked_cell].center
+        mark_size = tuple((int(x * 0.8) for x in self.grid_rects[clicked_cell].size))
+        # noinspection PyTypeChecker
+        self.marks.add(Mark(self.turn_player, mark_position, mark_size))
+
+    def finish_game(self) -> None:
+        """
+        Update the finished state and messages when there is a winner.
+        """
+        self.finished = True
+        (
+            self.finished_text,
+            self.finished_text_rect,
+            self.reset_text,
+            self.reset_text_rect
+        ) = self.generate_winner_message()
 
     def get_clicked_cell(self) -> Optional[int]:
         """
@@ -136,6 +167,7 @@ class GameController:
         Switch the turn between players.
         """
         self.turn_player = self.P2_MARK if self.turn_player == self.P1_MARK else self.P1_MARK
+        self.turn_text, self.turn_text_rect = self.generate_turn_message()
 
     @staticmethod
     def update_game_state() -> None:
@@ -148,20 +180,23 @@ class GameController:
         """
         Draw the game grid, player marks, and additional messages on the screen.
         """
-        # self.screen.blit(self.bg_surf, self.bg_rect)
-        self.screen.fill("white")
-        for grid_rect in self.grid_rects:
-            pg.draw.rect(self.screen, pg.Color("green"), grid_rect)
-        for i, grid_pos in enumerate(self.game_state):
-            if grid_pos == self.P1_MARK:
-                self.screen.blit(self.x_surf, self.grid_rects[i])
-            elif grid_pos == self.P2_MARK:
-                self.screen.blit(self.o_surf, self.grid_rects[i])
+        self.screen.blit(self.bg_surf, self.bg_rect)
+
+        # # Create a transparent surface with the same size as the screen
+        # transparent_surface = pg.Surface(self.screen.get_size(), pg.SRCALPHA)
+        # transparent_surface.fill((0, 0, 0, 0))  # Fills the surface with transparent black
+        # for grid_rect in self.grid_rects:
+        #     pg.draw.rect(transparent_surface, pg.Color(0, 255, 0, 128),
+        #                  grid_rect)  # 128 is the alpha value for 50% transparency
+        # self.screen.blit(transparent_surface, (0, 0))  # Blit the transparent surface onto the screen
+
+        self.marks.draw(self.screen)
 
         if self.finished:
-            self.draw_winner_message()
+            self.screen.blit(self.finished_text, self.finished_text_rect)
+            self.screen.blit(self.reset_text, self.reset_text_rect)
         else:
-            self.draw_turn_message()
+            self.screen.blit(self.turn_text, self.turn_text_rect)
 
         self.mouse_group.draw(self.screen)
 
@@ -181,32 +216,6 @@ class GameController:
             f"IMPOSSIBLE_CONDITIONS: {self.impossible_conditions}",
             f"SELF.WINNER: {self.winner}"
         )
-
-    def generate_grid_rects(self) -> List[pg.Rect]:
-        """
-        Generate and return the list of Rect objects representing the game grid cells.
-        :return: list[pg.Rect]: List of Rect objects.
-        """
-        screen_width = self.screen.get_width()
-        screen_height = self.screen.get_height()
-
-        rects = list()
-        square_size = screen_height // 8
-        margin = square_size // 5
-
-        grid_width = 3 * (square_size + margin) - margin
-        grid_height = 3 * (square_size + margin) - margin
-
-        start_x = (screen_width - grid_width) // 2
-        start_y = (screen_height - grid_height) // 2
-
-        for row in range(3):
-            for col in range(3):
-                x = start_x + col * (square_size + margin)
-                y = start_y + row * (square_size + margin)
-                rects.append(pg.Rect(x, y, square_size, square_size))
-
-        return rects
 
     def check_win(self) -> str:
         """
@@ -242,6 +251,7 @@ class GameController:
         """
         Reset the game state and conditions for a new game.
         """
+        self.marks.empty()
         self.p1_win_conditions = self.P1_WIN_CONDITIONS.copy()
         self.p2_win_conditions = self.P2_WIN_CONDITIONS.copy()
         self.impossible_conditions = set()
@@ -251,26 +261,89 @@ class GameController:
         self.finished = False
         self.winner = self.EMPTY_CELL
 
-    def draw_turn_message(self) -> None:
+    def generate_grid_rects(self) -> List[pg.Rect]:
         """
-        Draw the message indicating the current player's turn.
+        Generate and return the list of Rect objects representing the game grid cells.
+        :return: List[pg.Rect]: List of Rect objects.
         """
+        screen_width = self.screen.get_width()
+        screen_height = self.screen.get_height()
+
+        rects = list()
+        square_size = screen_width // 8
+        margin = square_size // 15
+
+        grid_width = 3 * (square_size + margin) - margin
+        grid_height = 3 * (square_size + margin) - margin
+
+        start_x = (screen_width - grid_width) // 2
+        start_y = (screen_height - grid_height) // 2
+
+        for row in range(3):
+            for col in range(3):
+                x = start_x + col * (square_size + margin)
+                y = start_y + row * (square_size + margin)
+                rects.append(pg.Rect(x, y, square_size, square_size))
+
+        return rects
+
+    def generate_background(self) -> Tuple[pg.Surface, pg.Rect]:
+        """
+        Load and resize the background image to maintain its aspect ratio and fill the screen.
+        :return: Tuple[pg.Surface, pg.Rect]: A tuple containing the resized background surface and its rectangle.
+        """
+        original_bg_surf = pg.image.load("src/assets/imgs/bg.png")
+        aspect_ratio = original_bg_surf.get_width() / original_bg_surf.get_height()
+
+        new_width = self.screen.get_width()
+        new_height = int(new_width / aspect_ratio)
+
+        bg_surf = pg.transform.scale(original_bg_surf, (new_width, new_height))
+        bg_rect = bg_surf.get_rect()
+        bg_rect.center = (self.screen.get_width() // 2, self.screen.get_height() // 2)
+
+        return bg_surf, bg_rect
+
+    def generate_turn_message(self) -> Tuple[pg.Surface, pg.Rect]:
+        turn_message_pos = (self.screen.get_width() // 2, self.screen.get_height() // 7)
         turn_text = self.font.render(f"Player {self.turn_player}'s Turn", pg.Color("black"), size=24)[0]
-        turn_rect = turn_text.get_rect(midbottom=(self.screen.get_width() // 2, 180))
-        self.screen.blit(turn_text, turn_rect)
+        turn_text_rect = turn_text.get_rect(midbottom=turn_message_pos)
 
-    def draw_winner_message(self) -> None:
+        return turn_text, turn_text_rect
+
+    def generate_winner_message(self) -> Tuple[pg.Surface, pg.Rect, pg.Surface, pg.Rect]:
         """
-        Draw the message announcing the winner or a tie.
+        Generate surfaces and rectangles for the winner and reset messages.
+        :return Tuple[pg.Surface, pg.Rect, pg.Surface, pg.Rect]: A tuple containing the background surface, text rect...
+        The winner message is displayed in the center of the screen, and the reset message is positioned below it.
+        Both messages have a white background for better readability.
         """
+        def generate_message_surface(message: str) -> Tuple[pg.Surface, pg.Rect]:
+            """
+            Generate a surface and rectangle for a given message.
+            :param message: The message to be displayed.
+            :return Tuple[pg.Surface, pg.Rect]: A tuple containing the background surface and text rectangle.
+            The message surface has a white background for better readability.
+            """
+            text_surface, text_rect = self.font.render(message, pg.Color("black"))
+            message_surface = pg.Surface((text_rect.width + 30, text_rect.height + 30))
+            message_surface.fill(pg.Color("white"))
+            text_position = ((message_surface.get_width() - text_surface.get_width()) // 2,
+                             (message_surface.get_height() - text_surface.get_height()) // 2)
+            message_surface.blit(text_surface, text_position)
+            message_rect = message_surface.get_rect()
+
+            return message_surface, message_rect
+
         if self.winner in (self.P1_MARK, self.P2_MARK):
-            text, text_rect = self.font.render(f"Player {self.winner} wins!", pg.Color("black"))
+            text = f"Player {self.winner} wins!"
         else:
-            text, text_rect = self.font.render("It's a tie!", pg.Color("black"))
+            text = "It's a tie!"
 
-        text_rect.center = (self.screen.get_width() // 2, self.screen.get_height() // 2)
-        self.screen.blit(text, text_rect)
+        finished_background_surface, finished_text_rect = generate_message_surface(text)
+        finished_text_rect.center = (self.screen.get_width() // 2, self.screen.get_height() // 2)
 
-        reset_text, reset_rect = self.font.render("Press 'r' to reset", pg.Color("black"))
-        reset_rect.center = (self.screen.get_width() // 2, text_rect.bottom + 20)
-        self.screen.blit(reset_text, reset_rect)
+        reset_background_surface, reset_text_rect = generate_message_surface("Press 'r' to reset")
+        reset_text_rect.center = (self.screen.get_width() // 2, finished_text_rect.bottom + 20)
+
+        return finished_background_surface, finished_text_rect, reset_background_surface, reset_text_rect
